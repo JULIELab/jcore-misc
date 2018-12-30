871b8879a2d09c9f62d1ca94affb730e6d6efdd4
khituras <chew@gmx.net>
2018-12-30 10:45:19 +0100

    Now the root POM is correctly handeled (setting the new version, letting its parent alone).

--- jcore-version-normalizer/src/main/kotlin/de/julielab/jcore/management/VersionNormalizer.kt
+++ jcore-version-normalizer/src/main/kotlin/de/julielab/jcore/management/VersionNormalizer.kt
@@ -4,6 +4,8 @@ import com.ximpleware.AutoPilot
 import com.ximpleware.VTDGen
 import com.ximpleware.VTDNav
 import com.ximpleware.XMLModifier
+import de.julielab.utilities.aether.AetherUtilities
+import de.julielab.utilities.aether.MavenArtifact
 import de.julielab.utilities.aether.MavenProjectUtilities
 import de.julielab.xml.JulieXMLConstants
 import de.julielab.xml.JulieXMLTools
@@ -13,55 +15,79 @@ import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
 
-    if (args.size != 2) {
-        println("Usage: VersionNormalizer <pom file> <new version>")
+    if (args.size != 3) {
+        println("Usage: VersionNormalizer <pom file> <new version> <allow snapshots for external julielab dependencies>")
         exitProcess(1)
     }
 
-    val version: String = args[1]
     val file = File(args[0])
+    val version: String = args[1]
+    val allowSnapshotForExternalDeps = args[2].toBoolean()
     val mavenProperties = MavenProjectUtilities.getEffectivePomModel(file).properties
-    val artifactIdProperties2Version = mavenProperties.propertyNames().asSequence().map { o -> Pair((o as String).replace("-version", ""), "\${$o}" ) }.toMap()
+    val artifactIdProperties2Version = mavenProperties.propertyNames().asSequence().map { o -> Pair((o as String).replace("-version", ""), "\${$o}") }.toMap()
     val projectModules = MavenProjectUtilities.getProjectModules(file, true)
+    // Also add the root, i.e. the project's parent POM
+    projectModules.add(".")
     val artifactIds: Set<String> = projectModules.map { m -> File(Paths.get(m, "pom.xml").toString()) }.map { f -> MavenProjectUtilities.getRawPomModel(f) }.map { m -> m.artifactId }.toSet()
     for (module in projectModules) {
         val vg = VTDGen()
         val pomPath = Paths.get(module, "pom.xml").toString()
         vg.parseFile(pomPath, false)
         var nav = vg.nav
-        var ap = AutoPilot(nav)
-        ap.selectXPath("/project/parent/version")
-        var xm = XMLModifier(nav)
 
-        var i = ap.evalXPath()
-        if (i != -1) {
-            var currentIndex = nav.currentIndex
-            while (nav.getTokenType(currentIndex) != VTDNav.TOKEN_CHARACTER_DATA)
-                ++currentIndex
-            xm.updateToken(currentIndex, version)
-        }
-        ap.resetXPath()
-        ap.selectXPath("/project/version")
-        i = ap.evalXPath()
-        if (i != -1) {
-            xm.remove()
+        // Do not delete the custom version for the root POM or change its parent -  this is the one location
+        // that determines the actual
+        // version for all the modules.
+        val xm = XMLModifier(nav)
+            var ap = AutoPilot(nav)
+        if (!module.equals(".")) {
+            ap.selectXPath("/project/parent/version")
+            var i = ap.evalXPath()
+            if (i != -1) {
+                var currentIndex = nav.text
+                xm.updateToken(currentIndex, version)
+            }
+            ap.resetXPath()
+            ap.selectXPath("/project/version")
+            i = ap.evalXPath()
+            if (i != -1) {
+                xm.remove()
+            }
+        } else {
+            // This is the root POM, set it to the specified version
+            ap.resetXPath()
+            ap.selectXPath("/project/version")
+           val i = ap.evalXPath()
+            if (i != -1) {
+                val textIndex = nav.text
+                xm.updateToken(textIndex, version)
+            }
         }
 
 
         val field1 = JulieXMLTools.createField(JulieXMLConstants.NAME, "artifactId", JulieXMLConstants.XPATH, "artifactId")
         val field2 = JulieXMLTools.createField(JulieXMLConstants.NAME, "version", JulieXMLConstants.XPATH, "version")
-        val fields = listOf(field1, field2)
+        val field3 = JulieXMLTools.createField(JulieXMLConstants.NAME, "groupId", JulieXMLConstants.XPATH, "groupId")
+        val fields = listOf(field1, field2, field3)
         val rowIt = JulieXMLTools.constructRowIterator(nav, "/project/dependencies/dependency[groupId='de.julielab']", fields, pomPath)
         for (row in rowIt) {
             if (row["version"] == null)
                 continue
-            if (artifactIds.contains(row.get("artifactId"))) {
-                val artifactId = row["artifactId"]
-                val versionExpression = if (!artifactIdProperties2Version.containsKey(artifactId)) version else artifactIdProperties2Version[artifactId]
+            if (row["groupId"]?.equals("de.julielab") != false) {
                 val vtdIndex: Int = row[JulieXMLConstants.VTD_INDEX] as Int
                 nav.push()
                 nav.recoverNode(vtdIndex)
-                // navigate to the version element
+                var versionExpression = ""
+                val artifactId = row["artifactId"]
+                if (artifactIds.contains(artifactId) || artifactIdProperties2Version.containsKey(artifactId)) {
+                    versionExpression = if (!artifactIdProperties2Version.containsKey(artifactId)) version else artifactIdProperties2Version[artifactId].orEmpty()
+                } else {
+                    // this is a julielab dependency but not in project-internal one
+                    val ma = MavenArtifact()
+                    ma.artifactId = artifactId as String
+                    ma.groupId = row["groupId"] as String
+                    AetherUtilities.getVersions(ma).filter { v -> !v.contains("SNAPSHOT") || allowSnapshotForExternalDeps }.forEach { v -> versionExpression = v }
+                }
                 JulieXMLTools.setElementText(nav, AutoPilot(nav), xm, "version", versionExpression)
                 nav.pop()
             }
